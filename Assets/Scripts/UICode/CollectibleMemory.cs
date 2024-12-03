de738b3fe9c9a765405e557b66df2b0e5356aac5
PythonXYZ <hilmirsg@gmail.com>
2024-12-03 10:10:42 +0100

    Making a Collectible Memory Script

---
+++ Assets/Scripts/UICode/CollectibleMemory.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollectibleMemory : MonoBehaviour
+{
+    public Scene Level1;
+    public Scene Level2;
+    public Scene Level3;
+    public Scene Level4;
+    public Scene Level5;
+    public Scene Level6;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

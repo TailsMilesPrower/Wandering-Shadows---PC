095ff523e4e8b2bc9faea79c847f9c03c60d129a
PythonXYZ <hilmirsg@gmail.com>
2024-12-04 11:48:55 +0100

    Edited the Settings Menu and Added Volume Control

---
+++ Assets/Scripts/Sound/AudioMixerManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer audioMixer;
+
+    public void SetMasterVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetSFXVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("SFXVolume", Mathf.Log10(sliderValue) * 20);
+    }
+
+    public void SetMusicVolume(float sliderValue)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(sliderValue) * 20);
+
+    }
+}
